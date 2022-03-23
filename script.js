09fb31c6d43d50a626ae18747f48d62e1b480041
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-03-22 20:07:12 -0500

    fixed error with querySelectorAll, changed to querySelector. updated issues with js, updated styling to make effects come from y axis

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controls');
 const sectBtn = document.querySelectorAll('.control');
-const allSections = document.querySelectorAll('.main-content');
+const allSections = document.querySelector('.main-content');
 
 function pageTransitions() {
     // button click active class
@@ -13,6 +13,26 @@ function pageTransitions() {
             this.className += ' active-btn';
         })
     }
+
+    // active sections class 
+    allSections.addEventListener('click', (e) =>{
+        const id = e.target.dataset.id;
+        if(id){
+            // remove selected from other buttons
+            sectBtns.forEach((btn) => {
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            // hide other sections
+            sections.forEach((section) =>{
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
 }
 
 pageTransitions();
