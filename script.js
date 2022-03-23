70f28dc30474fd34ff880f4bd49f8bf179e0a35c
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-03-22 19:37:55 -0500

    added function to replace active button

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelectorAll('.main-content');
+
+function pageTransitions() {
+    // button click active class
+    for (let i=0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', function(){
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '')
+            // this is referring to the pageTransitions function
+            this.className += ' active-btn';
+        })
+    }
+}
+
+pageTransitions();
